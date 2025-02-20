99d24585818a15d71e8d70e99f87ae539debd230
ChrisChrisChris <chrischrischris@users.noreply.github.com>
2025-02-19 19:35:07 -0500

    Fix smartling dnt

--- nx/blocks/loc/dnt/dnt.js
+++ nx/blocks/loc/dnt/dnt.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 import decorateTable from './decorateTable.js';
 import parseQuery from './parseQuery.js';
+import { processAltText, resetAltText } from './processAltText.js';
 
 function removeDntAttributes(document) {
   const dntEls = document.querySelectorAll('[translate="no"]');
@@ -37,18 +38,48 @@ export function resetIcons(doc) {
   });
 }
 
+function resetHrefs(doc, org, repo) {
+  const anchors = doc.querySelectorAll('[href^="/"]');
+  anchors.forEach((a) => {
+    const href = a.getAttribute('href');
+    a.href = `https://main--${repo}--${org}.aem.page${href}`;
+  });
+}
+
+const addDntWrapper = (node, dntContent) => {
+  node.innerHTML = node.innerHTML.replaceAll(dntContent, `<span translate="no" class="dnt-text">${dntContent}</span>`);
+};
+
+const findAndAddDntWrapper = (document, dntContent) => {
+  const contentMatches = document.evaluate(`//text()[contains(., "${dntContent}")]/..`, document, null, 0, null);
+  // eslint-disable-next-line no-underscore-dangle
+  contentMatches?._value?.nodes.forEach((node) => {
+    addDntWrapper(node, dntContent);
+  });
+};
+
+const unwrapDntContent = (document) => {
+  document.querySelectorAll('.dnt-text').forEach((dntSpan) => {
+    const spanParent = dntSpan.parentNode;
+    const textBefore = document.createTextNode(dntSpan.textContent);
+    const textAfter = document.createTextNode('');
+
+    spanParent.replaceChild(textAfter, dntSpan);
+    spanParent.insertBefore(textBefore, textAfter);
+    spanParent.normalize();
+  });
+};
+
 const addDntInfoToHtml = (html, dntRules) => {
   const parser = new DOMParser();
   const document = parser.parseFromString(html, 'text/html');
 
-  // TODO should this be done for generic dnt?
   makeHrefsRelative(document);
 
-  // TODO should this be done for generic dnt?
   document.querySelector('header')?.remove();
   document.querySelector('footer')?.remove();
 
-  dntRules.forEach((rules, block) => {
+  dntRules.docRules.forEach((rules, block) => {
     const blockEl = document.querySelector(`.${block}`);
     if (!blockEl) return;
     rules.forEach((rule) => {
@@ -56,28 +87,48 @@ const addDntInfoToHtml = (html, dntRules) => {
     });
   });
 
+  dntRules.contentRules.forEach((dntContent) => {
+    findAndAddDntWrapper(document, dntContent);
+  });
+
+  processAltText(document, addDntWrapper);
   return document.documentElement.outerHTML;
 };
 
 // TODO memoize
 function parseConfig(config) {
-  const data = config['custom-doc-rules']?.data || [];
-  const rules = new Map();
-  data.forEach((rule) => {
+  const docRules = config['custom-doc-rules']?.data || [];
+  const contentRules = config['dnt-content-rules']?.data || [];
+
+  const rules = {
+    docRules: new Map(),
+    contentRules: [],
+  };
+
+  docRules.forEach((rule) => {
     const blockList = rule.block.split(',').map((block) => block.trim());
     blockList.forEach((block) => {
-      const blockRules = rules.get(block) || [];
+      const blockRules = rules.docRules.get(block) || [];
       blockRules.push(rule.rule);
-      rules.set(block, blockRules);
+      rules.docRules.set(block, blockRules);
     });
   });
+
+  contentRules.forEach((contentRule) => {
+    rules.contentRules.push(contentRule.content);
+  });
+
   return rules;
 }
 
 export async function removeDnt(html, org, repo, { fileType = 'html' } = {}) {
   const parser = new DOMParser();
   const document = parser.parseFromString(html, 'text/html');
+
+  unwrapDntContent(document);
+  resetAltText(document);
   resetIcons(document);
+  resetHrefs(document, org, repo);
   removeDntAttributes(document);
   if (fileType === 'json') {
     const { html2json } = await import('./json2html.js');
@@ -86,7 +137,7 @@ export async function removeDnt(html, org, repo, { fileType = 'html' } = {}) {
   return document.documentElement.outerHTML;
 }
 
-export async function addDnt(inputText, config, { fileType = 'html', reset = false } = {}) {
+export async function addDnt(inputText, config, { fileType = 'html' } = {}) {
   let html = inputText;
   const rules = parseConfig(config);
 
